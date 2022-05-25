cfd844cc76f543d1f344873c4faee29370ceea31
Sushrut <41235363+sushrutmishra27@users.noreply.github.com>
2022-05-25 21:58:45 +0530

    Update DataLocation.sol

--- DataLocation.sol
+++ DataLocation.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 contract DataLocations {
     uint[] public arr;
-    mapping(uint => address) map;
+    mapping (uint => address) map;
     struct MyStruct {
         uint foo;
     }
